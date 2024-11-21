6a90562fe8724c15f319d816b6c96a916c67eee6
Dan Lacramioara <danlacramioara@gmail.com>
2024-11-21 12:36:43 +0200

    Your message

--- demo/frontend/src/App.js
+++ demo/frontend/src/App.js
@@ -1,20 +1,6 @@
-// src/App.js
-import React from 'react';
-import './App.css';
-import UserList from './components/UserList';  // Import the UserList component
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Pet Connect App</h1>
-      </header>
-      <UserList /> {/* Display the user list */}
-    </div>
-  );
-}
-
 import React from "react";
+import "./App.css";
+import UserList from "./components/UserList"; // Import the UserList component
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import PetProfiles from "./pages/PetProfiles";
@@ -23,19 +9,33 @@ import Signup from "./pages/Signup";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-const App = () => {
+function App() {
   return (
     <Router>
+      {/* Header Section */}
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pets" element={<PetProfiles />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+
+      {/* Main Content */}
+      <main>
+        <header className="App-header">
+          <h1>Pet Connect App</h1>
+        </header>
+        <UserList /> {/* Display the user list */}
+        
+        {/* Define Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pets" element={<PetProfiles />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </main>
+
+      {/* Footer Section */}
       <Footer />
     </Router>
   );
-};
+}
 
 export default App;
+
